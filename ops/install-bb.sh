6d4fd7e92a49017083157d946b4295f752d724ed
shivekkhurana <khuranashivek@outlook.com>
2020-05-19 16:35:41 +0530

    Remove ls in instal-bb and add attachement type

--- ops/install-bb.sh
+++ ops/install-bb.sh
@@ -7,7 +7,6 @@ esac
 curl -L "https://github.com/borkdude/babashka/releases/download/v0.0.91/babashka-0.0.91-$platform-amd64.zip" -o bb.zip
 unzip -qqo bb.zip
 rm bb.zip
-ls
 chmod +x ./bb
 
 ./bb --version
